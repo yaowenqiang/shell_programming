3b8e8ac8b2630894e9c038226ad8d91e417593be
yaowenqiang <yaowenqiang111@163.com>
2019-07-28 12:11:14 +0800

    add tests demo

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+[ -e /etc/passwd ]
+
+# File operators(tests
+# -d FILE True if file is a directory
+# -e FILE True if file exists
+# -f FILE True if file exists and is a regular file
+# -r FILE True if file is readable by you
+# -s FILE True if file exists and is not empty
+# -w FILE True if file is writable by you
+# -x File True if the file is executable by you
+# man test
+
+# String operators(tests)
+# -z STRING  True if string is empty
+# -n STRING  True if string is not empty
+# STRING1 = STRING2
+# True if the strings are euqal
+# STRING1 != STRING2
+# True if the strings are not euqal
+
+# Arthmetic operators(tests)
+
+# arg1 -eq arg2 True if arg1 is equal to arg2
+# arg1 -ne arg2 True if arg1 is not equal to arg2
+
+# arg1 -lt arg2 True if arg1 is less then arg2
+# arg1 -le arg2 True if arg1 is less then or equal to arg2
+
+# arg1 -gt arg2 True if arg1 is greater then arg2
+# arg1 -ge arg2 True if arg1 is greater then or equal to arg2
+
+
