4f2e28923b5ad324157cf7730c2d8550626c8704
yaowenqiang <yaowenqiang111@163.com>
2019-07-28 14:02:18 +0800

    add exit status demo

--- exit_status_demo.sh
+++ exit_status_demo.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 HOST="baidu.com"
 ping -c 1 $HOST
+RETURN_CODE=$?
 if [ "$?" -eq "0" ]
+#if [ "$RETURN_CODE" -ne "0" ]
+# if [ "$?" -ne "0" ]
+# ping -c 1 $HOST && echo "$HOST reachable."
+# ping -c 1 $HOST || echo "$HOST unreachable."
 then
     echo "$HOST reachable."
 else
     echo "$HOST unreachable."
+    exit 1
 fi
 
+exit 0
