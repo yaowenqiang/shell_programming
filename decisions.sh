86b79c870058505cc0a63d97d354cdc3866810e6
yaowenqiang <yaowenqiang111@163.com>
2019-07-28 12:19:21 +0800

    add else and elif demo

--- decisions.sh
+++ decisions.sh
@@ -6,12 +6,23 @@
 #    command 2
 #    command 3
 #    command 4
+#elif [ condition-is-true ]
+#then
+#    command 5
+#else
+
+#    command 6
 #fi
 
 MY_SHELL="bash"
 
-if [ "$MY_SHELL" = "bash" ]
+if [ "$MY_SHELL" = "zsh" ]
 then
-    echo "You seem to like the bash shell."
+    echo "You seem to like the zsh shell."
+elif [ "$MY_SHELL" = "csh" ]
+then
+    echo "You seem to like the csh shell."
+else
+    echo "I don't know what shell you like."
 fi
 
