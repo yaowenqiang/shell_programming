d30a99fecaeccb76816b5a7afcd0daf23285854e
yaowenqiang <yaowenqiang111@163.com>
2019-07-28 12:15:40 +0800

    add if condition statement demo

---
+++ decisions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#if [ condition-is=true ]
+#then
+#    command 1
+#    command 2
+#    command 3
+#    command 4
+#fi
+
+MY_SHELL="bash"
+
+if [ "$MY_SHELL" = "bash" ]
+then
+    echo "You seem to like the bash shell."
+fi
+
